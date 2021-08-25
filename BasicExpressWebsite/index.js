55d060803dfc100f23ad035b43e7c7168706fc9f
ezikechris <synergyaffirm@gmail.com>
2021-08-25 16:33:47 +0100

    add about & Home Routes

--- BasicExpressWebsite/index.js
+++ BasicExpressWebsite/index.js
@@ -1,6 +1,17 @@
 
 const express = require('express');
+const path = require('path');
 const PORT = process.env.PORT || 5000;
 const server = express();
+
+server.set(
+    "views",
+    path.join(__dirname, "views")
+  );
+
+server.set('view engine', 'ejs');
+
+server.get('/about', function(req, res) {  res.render('about');});
+server.get('/', function(req, res) {  res.render('Home');});
 
 server.listen(PORT, () => console.log(`Server Started On ${PORT}`));
