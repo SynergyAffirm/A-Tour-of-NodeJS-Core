5631c185247e8e2690eb13dddc7d8d188e640f46
ezikechris <synergyaffirm@gmail.com>
2021-08-25 17:17:03 +0100

    add Theme,Products,Section,Market,Error Routes

--- BasicExpressWebsite/index.js
+++ BasicExpressWebsite/index.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
@@ -7,11 +6,31 @@ const server = express();
 server.set(
     "views",
     path.join(__dirname, "views")
-  );
+);
 
 server.set('view engine', 'ejs');
 
-server.get('/about', function(req, res) {  res.render('about');});
-server.get('/', function(req, res) {  res.render('Home');});
+server.get('/about', function(req, res) {  
+    res.render('About');});
 
-server.listen(PORT, () => console.log(`Server Started On ${PORT}`));
+server.get('/', function(req, res) {  
+    res.render('Home');});
+
+server.get('/about', function(req, res) {  
+    res.render('Theme');});
+
+server.get('/', function(req, res) {  
+    res.render('Section');});
+
+server.get('/about', function(req, res) {  
+    res.render('Products');});
+
+server.get('/', function(req, res) {  
+    res.render('Market');});
+    
+server.get('*', function(req, res) {
+    res.render('Error')
+});
+
+server.listen(PORT, () => 
+   console.log(`Server Started On ${PORT}`));
